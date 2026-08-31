0d162aa00563c788800176591882178f7a4f2f95
George_Petrogiannopoulos <62185047+ch3ckkm8@users.noreply.github.com>
2026-09-01 00:20:06 +0300

    Update credwalk.sh

--- credwalk.sh
+++ credwalk.sh
@@ -33,7 +33,7 @@ printf '%s\n' '
        based on a true story. 47 hosts. 8 services each. zero willingness for manual typing
 
           "works here"          "apparently here too"           "oh well"
-              (._.)                    (•̀ᴗ•́)و                     (⌐■_■)
+              (._.)                    (•̀ᴗ•́)و                    (⌐■_■)
               /|  \                    /|  \                     /|   \
              / \  \                   / \  \                    / \   \
 
